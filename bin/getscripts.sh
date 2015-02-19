e92a35ea2f3ceb98d23a09144cd04d298e000ba4
Peter von Kaehne <refdoc@crosswire.org>
2015-02-19 00:49:25 +0000

    first script downloads

---
+++ bin/getscripts.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+wget http://www.crosswire.org/svn/sword-tools/trunk/modules/python/usfm2osis.py
+wget http://www.crosswire.org/svn/sword-tools/trunk/modules/crossreferences/xreffix.pl
+wget http://www.crosswire.org/svn/sword-tools/trunk/modules/conf/confmaker.pl
+
