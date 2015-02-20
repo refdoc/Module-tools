9f16da7651aa1509e6c353f86b61919e3cd5a087
Peter von Kaehne <refdoc@crosswire.org>
2015-02-20 00:58:37 +0000

    fetch and first commit of scripts

--- bin/getscripts.sh
+++ bin/getscripts.sh
@@ -3,3 +3,6 @@ wget http://www.crosswire.org/svn/sword-tools/trunk/modules/python/usfm2osis.py
 wget http://www.crosswire.org/svn/sword-tools/trunk/modules/crossreferences/xreffix.pl
 wget http://www.crosswire.org/svn/sword-tools/trunk/modules/conf/confmaker.pl
 
+chmod +x usfm2osis.py xreffix.pl confmaker.pl
+git add usfm2osis.py xreffix.pl confmaker.pl
+git commit -m "Scripts fetched" usfm2osis.py xreffix.pl confmaker.pl
