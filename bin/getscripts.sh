f8c701ab48cc23d39e27f1dde4c8466b049c8d5d
Peter von Kaehne <refdoc@crosswire.org>
2015-07-17 14:24:59 +0200

    removed scripts from bin directory which are either obsolete or will got directly from the master repo via the makefile.

--- bin/getscripts.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-wget http://www.crosswire.org/svn/sword-tools/trunk/modules/python/usfm2osis.py
-wget http://www.crosswire.org/svn/sword-tools/trunk/modules/crossreferences/xreffix.pl
-wget http://www.crosswire.org/svn/sword-tools/trunk/modules/conf/confmaker.pl
-
-chmod +x usfm2osis.py xreffix.pl confmaker.pl
-git add usfm2osis.py xreffix.pl confmaker.pl
-git commit -m "Scripts fetched" usfm2osis.py xreffix.pl confmaker.pl
